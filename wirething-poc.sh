efaf63c4724fce7ff75147f393b918563ba543d2
Fabio Mierlo <fmierlo@users.noreply.github.com>
2024-03-06 16:38:59 -0300

    Added on_demand_peer_subscribe_usecase and enabled it by default 🪁

--- wirething-poc.sh
+++ wirething-poc.sh
@@ -1032,6 +1032,45 @@ function always_on_peer_subscribe_usecase() {
     esac
 }
 
+# on demand peer subscribe usecase
+
+function on_demand_peer_subscribe_usecase() {
+    action="${1}" && shift
+    case "${action}" in
+        deps)
+            echo "sleep"
+            ;;
+        init)
+            info "on_demand_peer_subscribe_usecase init"
+            WT_ON_DEMAND_PEER_SUBSCRIBE_ENABLED="${WT_ON_DEMAND_PEER_SUBSCRIBE_ENABLED:-true}"
+            WT_ON_DEMAND_PEER_SUBSCRIBE_START_DELAY="${WT_ON_DEMAND_PEER_SUBSCRIBE_START_DELAY:-1}" # 1 second
+            WT_ON_DEMAND_PEER_SUBSCRIBE_INTERVAL="${WT_ON_DEMAND_PEER_SUBSCRIBE_INTERVAL:-60}" # 60 second
+            ;;
+        start)
+            if [[ "${WT_ON_DEMAND_PEER_SUBSCRIBE_ENABLED}" == "true" ]]
+            then
+                info "on_demand_peer_subscribe_usecase start $(short "${peer_id}")"
+                on_demand_peer_subscribe_usecase loop &
+            fi
+            ;;
+        loop)
+            debug "on_demand_peer_subscribe_usecase start $(short "${peer_id}") delay ${WT_ON_DEMAND_PEER_SUBSCRIBE_START_DELAY}"
+            sleep "${WT_ON_DEMAND_PEER_SUBSCRIBE_START_DELAY}"
+            while true
+            do
+                if [ "$(interface get handshake_timeout "${peer_id}")" == "true" ]
+                then
+                    wirething listen_peer_endpoint "${host_id}" "${peer_id}"
+                else
+                    debug "on_demand_peer_subscribe_usecase handshake_timeout $(short "${peer_id}") interval ${WT_ALWAYS_ON_PEER_SUBSCRIBE_INTERVAL} seconds"
+                    sleep "${WT_ON_DEMAND_PEER_SUBSCRIBE_INTERVAL}"
+                fi
+            done
+            debug "on_demand_peer_subscribe_usecase end $(short "${peer_id}")"
+            ;;
+    esac
+}
+
 # wirething main
 
 wt_type_list=(
@@ -1046,6 +1085,7 @@ wt_others_list=(
     wirething
     on_interval_punch_usecase
     always_on_peer_subscribe_usecase
+    on_demand_peer_subscribe_usecase
 )
 
 function wt_get_alias() {
@@ -1163,6 +1203,7 @@ function wirething_main() {
             for peer_id in ${peer_id_list}
             do
                 always_on_peer_subscribe_usecase start
+                on_demand_peer_subscribe_usecase start
             done
             ;;
         wait)
