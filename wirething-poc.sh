313184219c666cfb33f92a1276fa58c67eb64b1b
Fabio Mierlo <fmierlo@users.noreply.github.com>
2024-03-26 01:47:52 -0300

    Moved on_handshake_timeout_punch_usecase from main to museum 🚕

--- wirething-poc.sh
+++ wirething-poc.sh
@@ -1933,67 +1933,6 @@ function wirething() {
     esac
 }
 
-# on handshake timeout punch usecase
-
-function on_handshake_timeout_punch_usecase() {
-    local action="${1}" && shift
-    case "${action}" in
-        deps)
-            echo "cat sleep"
-            ;;
-        init)
-            info
-            WT_ON_HANDSHAKE_TIMEOUT_PUNCH_ENABLED="${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_ENABLED:-true}"
-            WT_ON_HANDSHAKE_TIMEOUT_PUNCH_START_DELAY="${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_START_DELAY:-45}" # 45 seconds
-            WT_ON_HANDSHAKE_TIMEOUT_PUNCH_INTERVAL="${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_INTERVAL:-15}" # 15 seconds
-            WT_ON_HANDSHAKE_TIMEOUT_PUNCH_MAX_BROADCAST_DAY="${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_MAX_BROADCAST_DAY:-250}"
-            WT_ON_HANDSHAKE_TIMEOUT_PUNCH_PID_FILE="${WT_EPHEMERAL_PATH}/on_handshake_timeout_punch_usecase.pid"
-            ;;
-        start)
-            local host_id="${1}" && shift
-            info "$(short "${host_id}")"
-
-            if [[ "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_ENABLED}" == "true" ]]
-            then
-                info "enabled"
-                on_handshake_timeout_punch_usecase loop &
-                echo "${!}" > "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_PID_FILE}"
-            else
-                info "disabled"
-            fi
-            ;;
-        loop)
-            info "pause before start: ${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_START_DELAY} seconds"
-            sleep "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_START_DELAY}"
-
-            PUNCH_PID="$(cat "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_PID_FILE}")"
-            SECONDS_DAY="$((24 * 60 * 60))"
-            BROADCAST_INTERVAL="$(("${SECONDS_DAY}" / "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_MAX_BROADCAST_DAY}" * "${peer_id_count}" ))"
-
-            while true
-            do
-                if [ "$(interface get handshake_timeouted "latest")" == "true" ]
-                then
-                    info "handshake_timeout is true"
-                    if wirething punch_host_endpoint
-                    then
-                        wirething broadcast_host_endpoint "${host_id}" "${peer_id_list}" &
-                        info "pause after broadcast_host_endpoint: ${BROADCAST_INTERVAL} seconds"
-                        sleep "${BROADCAST_INTERVAL}"
-                    else
-                        info "pause after punch_host_endpoint error: ${WT_PAUSE_AFTER_ERROR} seconds"
-                        sleep "${WT_PAUSE_AFTER_ERROR}"
-                    fi
-                else
-                    debug "pause: ${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_INTERVAL} seconds"
-                    sleep "${WT_ON_HANDSHAKE_TIMEOUT_PUNCH_INTERVAL}"
-                fi
-            done
-            info "end"
-            ;;
-    esac
-}
-
 # host status usecase
 
 function host_status_usecase() {
@@ -2234,7 +2173,6 @@ wt_others_list=(
     utils
     udp
     wirething
-    on_handshake_timeout_punch_usecase
     host_status_usecase
     peer_status_usecase
 )
@@ -2355,7 +2293,6 @@ function wirething_main() {
             local peer_id_list="$(interface get peer_id_list)"
             local peer_id_count="$(interface get peer_id_list | wc -l)"
 
-            on_handshake_timeout_punch_usecase start "${_host_id}"
             host_status_usecase start "${_host_id}"
 
             for _peer_id in ${peer_id_list}
