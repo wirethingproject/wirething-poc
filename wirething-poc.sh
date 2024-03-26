1ba3bcbbe2b7dd9289a6b4358d65d4ca3ce10ca1
Fabio Mierlo <fmierlo@users.noreply.github.com>
2024-03-26 01:30:57 -0300

    Added host status usecase 💺

--- wirething-poc.sh
+++ wirething-poc.sh
@@ -849,6 +849,7 @@ function wireproxy_interface() {
             then
                 touch "${WIREPROXY_RELOAD_FILE}"
                 wireproxy_interface stop
+                sleep 5 # TODO replace this sleep with a wait for wireproxy be ready
             fi
             ;;
         loop)
@@ -2044,6 +2045,141 @@ function on_handshake_timeout_punch_usecase() {
     esac
 }
 
+# host status usecase
+
+function host_status_usecase() {
+    local action="${1}" && shift
+    case "${action}" in
+        deps)
+            echo "sleep"
+            ;;
+        init)
+            info
+            WT_HOST_OFFLINE_ENABLED="${WT_HOST_OFFLINE_ENABLED:-true}"
+            WT_HOST_OFFLINE_START_DELAY="${WT_HOST_OFFLINE_START_DELAY:-20}" # 20 seconds
+            WT_HOST_OFFLINE_INTERVAL="${WT_HOST_OFFLINE_INTERVAL:-30}" # 30 seconds
+
+            WT_HOST_OFFLINE_ENSURE_INTERVAL="${WT_HOST_OFFLINE_ENSURE_INTERVAL:-60}" # 1 minute
+
+            WT_HOST_OFFLINE_FETCH_SINCE="${WT_HOST_OFFLINE_FETCH_SINCE:-60}" # 1 minute
+            WT_HOST_OFFLINE_FETCH_INTERVAL="${WT_HOST_OFFLINE_FETCH_INTERVAL:-30}" # 30 seconds
+
+            WT_HOST_OFFLINE_PUNCH_PID_FILE="${WT_EPHEMERAL_PATH}/host_status_usecase.pid"
+            ;;
+        start)
+            local host_id="${1}" && shift
+            info "$(short "${host_id}")"
+
+            if [[ "${WT_HOST_OFFLINE_ENABLED}" == "true" ]]
+            then
+                info "enabled"
+                host_status_usecase loop &
+                echo "${!}" > "${WT_HOST_OFFLINE_PUNCH_PID_FILE}"
+            else
+                info "disabled"
+            fi
+            ;;
+        online)
+            info
+            while [ "$(interface get host_status "${host_id}")" == "online" ]
+            do
+                debug "pause: ${WT_HOST_OFFLINE_INTERVAL} seconds"
+                sleep "${WT_HOST_OFFLINE_INTERVAL}"
+            done
+            ;;
+        offline)
+            info
+
+            local next_ensure="0"
+
+            while [ "$(interface get host_status "${host_id}")" == "offline" ]
+            do
+                if [[ $(epoch) -gt ${next_ensure} ]]
+                then
+                    local status="online"
+
+                    if [ "$(pubsub status)" == "offline" ]
+                    then
+                        info "pubsub status: offline"
+                        status="offline"
+                    else
+                        read host_endpoint < <(wirething get host_endpoint)
+                        read host_port < <(wirething get host_port)
+
+                        wirething set host_port "0"
+
+                        if [ "$(punch status "${host_port}" "${host_endpoint}")" == "online" ]
+                        then
+                            wirething set host_port "${host_port}"
+                        else
+                            info "punch status: offline"
+                            status="offline"
+
+                            if wirething punch_host_endpoint
+                            then
+                                wirething broadcast_host_endpoint "${host_id}" "${peer_id_list}" &
+                                status="online"
+                            else
+                                wirething set host_port "${host_port}"
+                            fi
+                        fi
+                    fi
+
+                    if [ "${status}" == "online" ]
+                    then
+                        for _peer_id in ${peer_id_list}
+                        do
+                            if ! wirething ensure_host_endpoint_is_published "${host_id}" "${_peer_id}"
+                            then
+                                status="offline"
+                            fi
+                        done
+                    fi
+
+                    if [ "${status}" == "online" ]
+                    then
+                        next_ensure="$(($(epoch) + "${WT_HOST_OFFLINE_ENSURE_INTERVAL}"))"
+                        info "next ensure_host_endpoint_is_published in $((${next_ensure} - $(epoch))) seconds"
+                    fi
+
+                    if [ "${status}" == "offline" ]
+                    then
+                        info "pause after error: ${WT_PAUSE_AFTER_ERROR} seconds"
+                        sleep "${WT_PAUSE_AFTER_ERROR}"
+                        continue
+                    fi
+                fi
+
+                debug "pause after fetch_host_endpoint: ${WT_HOST_OFFLINE_FETCH_INTERVAL} seconds"
+                sleep "${WT_HOST_OFFLINE_FETCH_INTERVAL}"
+            done
+            ;;
+        loop)
+            info "pause before start: ${WT_HOST_OFFLINE_START_DELAY} seconds"
+            sleep "${WT_HOST_OFFLINE_START_DELAY}"
+
+            PUNCH_PID="$(cat "${WT_HOST_OFFLINE_PUNCH_PID_FILE}")"
+
+            while true
+            do
+                case "$(interface get host_status "${host_id}")" in
+                    online)
+                        host_status_usecase online
+                        ;;
+                    offline)
+                        host_status_usecase offline
+                        ;;
+                    *)
+                        error "invalid host status"
+                esac
+
+            done
+
+            info "end"
+            ;;
+    esac
+}
+
 # peer status usecase
 
 function peer_status_usecase() {
@@ -2156,6 +2292,7 @@ wt_others_list=(
     wirething
     on_interval_punch_usecase
     on_handshake_timeout_punch_usecase
+    host_status_usecase
     peer_status_usecase
 )
 
@@ -2277,6 +2414,7 @@ function wirething_main() {
 
             on_interval_punch_usecase start "${_host_id}"
             on_handshake_timeout_punch_usecase start "${_host_id}"
+            host_status_usecase start "${_host_id}"
 
             for _peer_id in ${peer_id_list}
             do
